08492523768a95d0f55a9396d884fd53bb9ea610
sali1502 <sali1502@student.miun.se>
2024-07-24 18:47:38 +0200

    Formateringar

--- src/js/token.js
+++ src/js/token.js
@@ -1,7 +1,8 @@
 window.onload = init;
 async function init() {
     const jwtToken = localStorage.getItem("jwtToken");
-    if(!jwtToken) {
-        window.location.href="index.html"
+    if (!jwtToken) {
+        window.location.href = "index.html"
     }
 };
+
